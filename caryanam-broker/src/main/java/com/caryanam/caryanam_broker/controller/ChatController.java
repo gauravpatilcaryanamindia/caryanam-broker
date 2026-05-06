7258e0dd9660ed9647273227fd953473dbf3b707
gauravsantoshpatil <gauravpatil742002@gamil.com>
2026-05-06 14:06:01 +0530

    add

--- caryanam-broker/src/main/java/com/caryanam/caryanam_broker/controller/ChatController.java
+++ caryanam-broker/src/main/java/com/caryanam/caryanam_broker/controller/ChatController.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/chat")
+@RequestMapping("/api/chat")
 @RequiredArgsConstructor
 public class ChatController {
     @Autowired
