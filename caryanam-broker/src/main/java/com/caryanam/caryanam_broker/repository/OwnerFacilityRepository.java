a9b51aebacf650eda9b23791637a9290577b7717
gauravsantoshpatil <gauravpatil742002@gamil.com>
2026-05-08 13:03:51 +0530

    add

--- caryanam-broker/src/main/java/com/caryanam/caryanam_broker/repository/OwnerFacilityRepository.java
+++ caryanam-broker/src/main/java/com/caryanam/caryanam_broker/repository/OwnerFacilityRepository.java
@@ -1,6 +1,7 @@
 package com.caryanam.caryanam_broker.repository;
 
 import com.caryanam.caryanam_broker.entity.OwnerFacility;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,6 @@ import java.util.List;
 public interface OwnerFacilityRepository extends JpaRepository<OwnerFacility, Long> {
 
     List<OwnerFacility> findByOwnerId(Long ownerId);
-
+    @Transactional
     void deleteByOwnerId(Long ownerId);
 }
--- caryanam-broker/src/main/java/com/caryanam/caryanam_broker/serviceimpl/OwnerFacilityServiceImpl.java
+++ caryanam-broker/src/main/java/com/caryanam/caryanam_broker/serviceimpl/OwnerFacilityServiceImpl.java
@@ -4,6 +4,7 @@ import com.caryanam.caryanam_broker.dto.OwnerFacilityRequest;
 import com.caryanam.caryanam_broker.entity.OwnerFacility;
 import com.caryanam.caryanam_broker.repository.OwnerFacilityRepository;
 import com.caryanam.caryanam_broker.service.OwnerFacilityService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class OwnerFacilityServiceImpl implements OwnerFacilityService {
 
     @Autowired
     private OwnerFacilityRepository repository;
-
+    @Transactional
     @Override
     public String saveFacilities(OwnerFacilityRequest request) {
 
